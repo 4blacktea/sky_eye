1c9c9a8bcb6bf4d429a576509080ab5dde58fdd6
4blacktea <81417218+4blacktea@users.noreply.github.com>
2021-04-04 02:18:43 +0800

    Update cron.py

--- src/cron.py
+++ src/cron.py
@@ -1,11 +1,12 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
 from apscheduler.triggers.cron import CronTrigger
 from apscheduler.triggers.combining import OrTrigger
+import os
 
-def job_function():
-        print("Hello World")
+def run():
+    os.system("./run.sh megvii.com")
 
 sched = BlockingScheduler()
-sched.add_job(job_function, CronTrigger.from_crontab('15 2 * * *'))
+sched.add_job(run, CronTrigger.from_crontab('15 2 * * *'))
 sched.start()
 
