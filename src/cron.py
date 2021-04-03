8f9d162fb35ca7708b0e767e6bcba86cfd317a9d
4blacktea <81417218+4blacktea@users.noreply.github.com>
2021-04-04 01:49:12 +0800

    Update cron.py

--- src/cron.py
+++ src/cron.py
@@ -1,18 +1,11 @@
-import datetime
 from apscheduler.schedulers.blocking import BlockingScheduler
-import os
+from apscheduler.triggers.cron import CronTrigger
+from apscheduler.triggers.combining import OrTrigger
 
+def job_function():
+        print("Hello World")
 
-scheduler = BlockingScheduler()   # 后台运行
+sched = BlockingScheduler()
+sched.add_job(job_function, CronTrigger.from_crontab('* * * * *'))
+sched.start()
 
-# 设置为每日15:30:30时执行一次调度程序
-@scheduler.scheduled_job("cron", day_of_week='*', hour='17', minute='30', second='35')
-def run():
-    os.system("./run.sh megvii.com")
-
-
-if __name__ == '__main__':
-    try:
-        scheduler.start()
-    except (KeyboardInterrupt, SystemExit):
-        scheduler.shutdown()
