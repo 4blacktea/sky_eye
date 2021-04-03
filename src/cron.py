850c16960886d86f63284a79df31e47706cbe86d
4blacktea <81417218+4blacktea@users.noreply.github.com>
2021-04-04 02:19:06 +0800

    Update cron.py

--- src/cron.py
+++ src/cron.py
@@ -7,6 +7,6 @@ def run():
     os.system("./run.sh megvii.com")
 
 sched = BlockingScheduler()
-sched.add_job(run, CronTrigger.from_crontab('15 2 * * *'))
+sched.add_job(run, CronTrigger.from_crontab('25 2 * * *'))
 sched.start()
 
