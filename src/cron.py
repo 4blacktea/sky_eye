7240a18b9d35f07b8539799fd1b30c6ba08fd6c0
4blacktea <2922834123@qq.com>
2021-04-04 13:10:19 +0800

    add cron setter
    add cron setter

--- src/cron.py
+++ src/cron.py
@@ -2,6 +2,32 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 from apscheduler.triggers.cron import CronTrigger
 from apscheduler.triggers.combining import OrTrigger
 import os
+import mysql.connector
+
+
+# 数据库连接
+def my_mysql():
+    mydb = mysql.connector.connect(
+        host="10.10.10.2",       # 数据库主机地址
+        port=3306,
+        user="root",    # 数据库用户名
+        passwd="adminermysql",   # 数据库密码
+        database="sky_eye"
+    )
+    mycursor = mydb.cursor(buffered=True)
+    return mycursor,mydb
+
+
+def get_corn():
+    mycursor,mydb = my_mysql()
+    cron_rules = []
+    select_sql = "select * from cron_rules where is_open=%s"
+    mycursor.execute(select_sql,("true",))
+    myresult = mycursor.fetchall()
+    for x in myresult:
+        cron_rules.append(x[0])
+    pass
+
 
 def run():
     os.system("./run.sh megvii.com")
