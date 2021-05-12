236295037bbc301690da2d797f9356b373e9b4ae
root <root@hwsrv-743137.hostwindsdns.com>
2021-05-12 09:02:13 +0000

    fix html

--- html/skins.js
+++ html/skins.js
@@ -3,4 +3,8 @@ for(var i=0;i<pics.length;i++){
     pics[i].innerHTML="<img src="+pics[i].innerText+"></img>"
 
 };
+var numofselect = document.getElementsByClassName("select").length;
+for(var i=0;i<numofselect;i++){
+    document.getElementsByClassName("select")[0].setAttribute("class","");console.log(i);
+};
 document.getElementsByName("where[0][op]")[0].innerHTML = document.getElementsByName("where[0][op]")[0].innerHTML+"<option>=</option>"
