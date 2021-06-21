175424a16bfbff6853a03553bfcdd7eb06f2c451
root <root@localhost.localdomain>
2021-06-21 14:25:13 +0800

    fix skin

--- html/skins.js
+++ html/skins.js
@@ -1,8 +1,9 @@
 var pics = document.querySelectorAll('[id$="screen_pic]"]');
 for(var i=0;i<pics.length;i++){
     var host = document.domain;
-    pics[i].innerHTML="<img src= http://"+host+":8000/"+pics[i].innerText+"></img>"
-
+    if(pics[i].innerText!="screen_pic"){
+        pics[i].innerHTML="<img src= http://"+host+":8000/"+pics[i].innerText+"></img>"
+    }
 };
 var numofselect = document.getElementsByClassName("select").length;
 for(var i=0;i<numofselect;i++){
