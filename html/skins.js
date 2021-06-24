6d474bcff2d0ccdcdf43cc63a0b770f3a8fd66f6
root <root@localhost.localdomain>
2021-06-24 15:16:28 +0800

    aaa

--- html/skins.js
+++ html/skins.js
@@ -1,12 +1,23 @@
 var pics = document.querySelectorAll('[id$="screen_pic]"]');
 for(var i=0;i<pics.length;i++){
-    var host = document.domain;
-    if(pics[i].innerText!="screen_pic"){
-        pics[i].innerHTML="<img src= http://"+host+":8000/"+pics[i].innerText+"></img>"
-    }
+	    var host = document.domain;
+	    if(pics[i].innerText!="screen_pic"){
+		            pics[i].innerHTML="<img src= http://"+host+":8000/"+pics[i].innerText+"></img>"
+		        }
 };
 var numofselect = document.getElementsByClassName("select").length;
 for(var i=0;i<numofselect;i++){
-    document.getElementsByClassName("select")[0].setAttribute("class","");console.log(i);
+        document.getElementsByClassName("select")[0].setAttribute("class","");console.log(i);
 };
-document.getElementsByName("where[0][op]")[0].innerHTML = document.getElementsByName("where[0][op]")[0].innerHTML+"<option>=</option>"
+var ii = 0;
+while(1){
+	if(document.getElementsByName("where["+ii+"][op]").length==0)
+	{
+		break;
+	}
+	document.getElementsByName("where["+ii+"][op]")[0].innerHTML = document.getElementsByName("where["+ii+"][op]")[0].innerHTML+"<option>=</option><option>!=</option><option>LIKE</option><oprion>NOT LIKE</option><option>REGEXP</option>"
+	ii=ii+1;
+
+}
+
+
